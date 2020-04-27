3ea81944d419ebbf01a8f8e4777bbc5a618dc0d6
Ethan Clarke <48656434+AwsumN00b@users.noreply.github.com>
2020-04-27 22:19:31 +0100

    Fixed quotesort.py and amended its mistakes
    I made the mistake of using a dictionary for quotesort.py, which ended up with it deleted lines with identical credits (If two quotes were credited "ugotaphonenumber, 2012", the script would overwrite the old one with the new one because dictionaries only allow one value per key)
    The script no longer does this, and I have painstakingly restored quotes it has removed due to my errors

--- quotesort.py
+++ quotesort.py
@@ -1,24 +1,28 @@
 #!/usr/bin/env python3
 
 #I thought finding quotes would be faster if they were ordered based on youtuber
-#This method wont overwrite the original just in case it fucks up
+#This method wont overwrite the original just in case it fucks up (first iteration of this script did fuck up)
 
 #the old file should be deleted or moved, and the new file that is made should be
 #renamed to quotes.txt
 
 import os
+import sys
 
-dict = {}
-with open("quotes.txt", "r") as f:
+quotes = []
+with open(sys.argv[1], "r") as f:
     for line in f.readlines():
         line = line.strip()
         splitline = line.split(" - ")
         try:
-            dict[splitline[1]] = line
+            quotes.append((splitline[0], splitline[1]))
         except IndexError:
-            dict[line] = line
+            quotes.append(line)
 
 with open("sorted_quotes.txt", "a") as f:
-    for key in sorted(dict):
+    for key in sorted(quotes, key=lambda x: x[1]):
         print(key)
-        f.write(dict[key] + "\r\n")
+        if type(key) == tuple:
+            f.write(str(key[0]) + " - " + str(key[1]) + "\r\n")
+        else:
+            f.write(str(key) + "\r\n")
